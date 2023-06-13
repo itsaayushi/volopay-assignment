4de86600e64e603197a8ad7f0656397bb2495dc9
itsaayushi <itsaayushiemail@gmail.com>
2023-06-13 14:01:43 +0530

    Minor Fix for route

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -4,11 +4,12 @@ import AllPage from "../pages/all";
 import YourPage from "../pages/your";
 import BlockedPage from "../pages/blocked";
 import Layout from "../Layout";
-
+import { Navigation } from "react-router-dom";
 function Myrouter() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Navigation to="/all" />} />
         <Route path="/all" element={<AllPage />} />
         <Route path="/your" element={<YourPage />} />
         <Route path="/blocked" element={<BlockedPage />} />
