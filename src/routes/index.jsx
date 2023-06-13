bb1bcb7c094d9f0fb80d3a0be33b92c4af4a89e9
itsaayushi <itsaayushiemail@gmail.com>
2023-06-13 14:05:02 +0530

    fix/replaced with new component correct component

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,15 +1,15 @@
 import React from "react";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Navigate } from "react-router-dom";
 import AllPage from "../pages/all";
 import YourPage from "../pages/your";
 import BlockedPage from "../pages/blocked";
 import Layout from "../Layout";
-import { Navigation } from "react-router-dom";
+
 function Myrouter() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route path="/" element={<Navigation to="/all" />} />
+        <Route path="/" element={<Navigate to="/all" />} />
         <Route path="/all" element={<AllPage />} />
         <Route path="/your" element={<YourPage />} />
         <Route path="/blocked" element={<BlockedPage />} />
